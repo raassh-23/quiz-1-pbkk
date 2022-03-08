982ea6f1cd1aa1aba89fa014267ad1b9616b6e61
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-03-08 19:07:17 +0700

    add placeholder for company and profile

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// ref: https://mdbootstrap.com/docs/standard/extended/back-to-top/
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
